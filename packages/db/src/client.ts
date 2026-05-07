54946c8a7a2aaad451570eb53b2ae82186ee39bd
RuiFerreira05 <ruimf.05@gmail.com>
2026-05-07 17:40:19 +0100

    allow query API

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,5 +2,13 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@baza/db/schemas";
 
 export const createDbClient = (connectionString: string) => {
-  return drizzle({ connection: connectionString, schema, logger: true });
+  return drizzle({
+    connection: connectionString, schema, logger: true, relations: {
+      ...schema.authRelations,
+      ...schema.profileRelations,
+      ...schema.groupRelations,
+      ...schema.eventRelations,
+      ...schema.planRelations,
+      ...schema.preferenceRelations,
+  }});
 };
