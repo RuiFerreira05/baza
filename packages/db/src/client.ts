c7d347e018254ae34f6112134b6729e08738064e
RuiFerreira05 <ruimf.05@gmail.com>
2026-05-10 18:45:42 +0100

    improve readibility in drizzle createClient

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,12 +3,16 @@ import * as schema from "@baza/db/schemas";
 
 export const createDbClient = (connectionString: string) => {
   return drizzle({
-    connection: connectionString, schema, logger: true, relations: {
+    connection: connectionString,
+    schema,
+    logger: true,
+    relations: {
       ...schema.authRelations,
       ...schema.profileRelations,
       ...schema.groupRelations,
       ...schema.eventRelations,
       ...schema.planRelations,
       ...schema.preferenceRelations,
-  }});
+    },
+  });
 };
